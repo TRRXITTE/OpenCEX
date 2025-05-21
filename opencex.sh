06134242ac5dc503d09a8c3f607ed8f80de5f34d
traaitt <traaitt@icloud.com>
2025-05-21 21:25:53 +0200

    Update opencex.sh

--- opencex.sh
+++ opencex.sh
@@ -18,10 +18,10 @@ fi
 
 mkdir /app/opencex -p
 cd /app/opencex || exit
-git clone  https://github.com/Polygant/OpenCEX-backend.git ./backend
-git clone  https://github.com/Polygant/OpenCEX-frontend.git ./frontend
-git clone  https://github.com/Polygant/OpenCEX-static.git ./nuxt
-git clone  https://github.com/Polygant/OpenCEX-JS-admin.git ./admin
+git clone  https://github.com/fx-traaitt/OpenCEX-backend.git ./backend
+git clone  https://github.com/fx-traaitt/OpenCEX-frontend.git ./frontend
+git clone  https://github.com/fx-traaitt/OpenCEX-static.git ./nuxt
+git clone  https://github.com/fx-traaitt/OpenCEX-JS-admin.git ./admin
 
 echo "`cat <<YOLLOPUKKI
 
@@ -1145,7 +1145,7 @@ EOF
 # build hummingbot
 if [ "$IS_HUMMINGBOT_ENABLED" = "True" ]; then
 cd /app/opencex || exit
-git clone  https://github.com/Polygant/hummingbot.git ./hmbot
+git clone  https://github.com/fx-traaitt/hummingbot.git ./hmbot
 cd ./hmbot
 docker build -t hummingbot:latest -f Dockerfile --target=release .
 cat << EOF >> /app/opencex/docker-compose.yml
